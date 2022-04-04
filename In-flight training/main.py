c624a93d999fe6ed24ac9182bf9d31400cee497b
Dimitri <88735045+Iletaitmembre@users.noreply.github.com>
2022-04-05 01:11:56 +0300

    In-flight training
    Все необходимые для освоения планеты специальности можно условно разделить на инженерные и научные. Лететь на Марс долго, около полугода, экипаж необходимо занять тренировками по специальности. В адресной строке передается параметр специальность, в зависимости от которого выводится заголовок второго уровня Инженерные тренажеры или Научные симуляторы и схема расположения их на космическом корабле.
    
    Если в специальности участника миссии есть слово инженер или строитель, то его надо направить на соответствующие тренажёры и симуляторы и вывести соответствующую схему, если нет, то на другие.
    
    Создайте шаблон, расширяющий базовый, в котором в зависимости от переданного параметра отображаются различные значения заголовка и картинки, а также обработчик для адреса /training/<prof>.

---
+++ In-flight training/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    title = title
+    return render_template('base.html', title=title)
+
+
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('In-flight training.html', occupation=prof)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
